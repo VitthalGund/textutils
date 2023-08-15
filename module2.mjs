e70350a42646e2c32d6119ad251ce962f1c2e47b
Vitthal Gund <97181033+VitthalGund@users.noreply.github.com>
2023-08-15 10:59:52 +0530

    Delete module2.mjs

--- module2.mjs
+++
@@ -1,7 +0,0 @@
-import axs, { a, b, c, d } from './module1.mjs'
-console.log("These are imported Person's in this country!");
-console.log(axs);
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
