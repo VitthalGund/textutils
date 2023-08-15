549080a1ade54f52fafaa8ce08ff98de4871a456
Vitthal Gund <97181033+VitthalGund@users.noreply.github.com>
2023-08-15 06:11:00 +0000

    GitHub Icon Added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
   return (
     <>
       <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`} >
         <div className="container navbar-brand nav-justified">
-          <a className="navbar-brand" href="#">{props.title}</a>
+          <a className="navbar-brand" href="vitthalgund.github.io/textutils/">{props.title}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-left mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link active" aria-current="page" href="vitthalgund.github.io/textutils/">Home</a>
+              </li>
+              <li className="nav-item">
+                <Link to="https://github.com/VitthalGund"><i class="bi bi-github"></i></Link>
               </li>
               {/* <li className="nav-item">
                 <a className="nav-link active" href="/about">{props.aboutTitle}</a>
